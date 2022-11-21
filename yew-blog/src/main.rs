e0be64b347bf368bd51a2abe90eb45392605b28a
sntkn <mail.tsurumi@gmail.com>
2022-11-21 21:49:14 +0900

    add api endpoint

--- yew-blog/src/main.rs
+++ yew-blog/src/main.rs
@@ -1,4 +1,55 @@
+use gloo_net::http::Request;
+use serde::Deserialize;
 use yew::prelude::*;
+
+#[derive(Deserialize, Clone, PartialEq)]
+pub struct Post {
+    id: i32,
+    title: String,
+    body: String,
+    published: bool,
+}
+
+#[derive(Properties, PartialEq)]
+struct PostsListProps {
+    posts: Vec<Post>,
+    on_click: Callback<Post>,
+}
+
+#[function_component(PostList)]
+fn posts_list(PostsListProps { posts, on_click }: &PostsListProps) -> Html {
+    posts
+        .iter()
+        .map(|post| {
+            let on_post_select = {
+                let on_click = on_click.clone();
+                let post = post.clone();
+                Callback::from(move |_| on_click.emit(post.clone()))
+            };
+            html! {
+                <p onclick={on_post_select}>{
+                    format!("{} {}",
+                    post.id, post.title)
+                }</p>
+            }
+        })
+        .collect()
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct PostDetailProps {
+    post: Post,
+}
+
+#[function_component(PostDetail)]
+fn post_detail(PostDetailProps { post }: &PostDetailProps) -> Html {
+    html! {
+        <div>
+            <h3>{ post.title.clone() }</h3>
+            <p>{ post.body.clone() }</p>
+        </div>
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
