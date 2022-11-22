b2254311311224dcaf90b5ef1e672a1b4ff70eec
sntkn <mail.tsurumi@gmail.com>
2022-11-22 21:39:49 +0900

    api app

--- yew-blog/src/main.rs
+++ yew-blog/src/main.rs
@@ -53,8 +53,46 @@ fn post_detail(PostDetailProps { post }: &PostDetailProps) -> Html {
 
 #[function_component(App)]
 fn app() -> Html {
+    let posts = use_state(|| vec![]);
+    {
+        let posts = posts.clone();
+        use_effect_with_deps(
+            move |_| {
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_posts: Vec<Post> = Request::get("/posts")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    posts.set(fetched_posts);
+                });
+                || ()
+            },
+            (),
+        );
+    }
+    let selected_post = use_state(|| None);
+    let on_post_select = {
+        let selected_post = selected_post.clone();
+        Callback::from(move |post: Post| selected_post.set(Some(post)))
+    };
+    let detail = selected_post.as_ref().map(|post| {
+        html! {
+            <PostDetail post={post.clone()} />
+        }
+    });
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "My blog" }</h1>
+            <div>
+                <h3>{ "posts list" }</h3>
+                <PostList posts={(*posts).clone()} on_click={on_post_select.clone()} />
+            </div>
+            { for detail }
+        </>
     }
 }
 
