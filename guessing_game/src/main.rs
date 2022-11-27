1cb1f3ed76399ac139cf0f91194654d9e382431c
sntkn <mail.tsurumi@gmail.com>
2022-11-27 17:33:17 +0900

    change type

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,6 +17,9 @@ fn main() {
         .read_line(&mut guess)
         .expect("Faild to read line");
 
+    let guess:u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed {}", guess);
 
     match guess.cmp(&secret_number) {
@@ -24,5 +27,4 @@ fn main() {
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal => println!("You win!"),
     }
-
 }
