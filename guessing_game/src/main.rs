0a070521b6d26f68d0814c4ce8c0e57bd7b00395
sntkn <mail.tsurumi@gmail.com>
2022-11-27 17:26:10 +0900

    func match

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,13 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guessing number!");
 
-    let random_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", random_number);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
     
@@ -17,5 +18,11 @@ fn main() {
         .expect("Faild to read line");
 
     println!("You guessed {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
 }
