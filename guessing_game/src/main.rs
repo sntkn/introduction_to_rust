7d8d83a610fdfd87d19a67c76aa7f793c4efeb32
sntkn <mail.tsurumi@gmail.com>
2022-11-27 17:11:30 +0900

    input console

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing number!");
+
+    println!("Please input your guess.");
+    
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Faild to read line");
+
+    println!("You guessed {}", guess);
+
 }
