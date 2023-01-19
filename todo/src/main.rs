e8f879f75872ab037256a67bacce092b5ef9d396
sntkn <mail.tsurumi@gmail.com>
2023-01-19 23:09:17 +0900

    add post method

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,6 +1,12 @@
-use axum::{routing::get, Router};
-use std::net::SocketAddr;
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use std::env;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
@@ -9,8 +15,11 @@ async fn main() {
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -20,4 +29,27 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world!"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>, // request は deserialize
+) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // response は serialize
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
