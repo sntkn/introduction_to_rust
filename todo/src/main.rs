ccd69d051b23b7ec2c2992bac63e55b0c3c9197f
sntkn <mail.tsurumi@gmail.com>
2023-01-19 22:50:01 +0900

    add logging

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,8 +1,14 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    // logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
