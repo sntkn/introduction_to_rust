6e74b2bb8f6451da73e6d4a2fbd78c6a99ac1acd
sntkn <mail.tsurumi@gmail.com>
2023-01-23 22:13:27 +0900

    Devide code

---
+++ todo/src/handlers.rs
@@ -0,0 +1,11 @@
+use crate::repositories::{CreateTodo, TodoRepository};
+use axum::{extract::Extension, http::StatusCode, response::IntoResponse, Json};
+use std::sync::Arc;
+
+pub async fn create_todo<T: TodoRepository>(
+    Json(payload): Json<CreateTodo>, // request は deserialize
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    let todo = repository.create(payload);
+    (StatusCode::CREATED, Json(todo)) // response は serialize
+}
