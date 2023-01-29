0ffe8a9da79f892e3ea72ca8c273031bf963df06
sntkn <mail.tsurumi@gmail.com>
2023-01-29 22:03:18 +0900

    Move todo handler common logic

--- todo/todo-api/src/handlers.rs
+++ todo/todo-api/src/handlers.rs
@@ -1,1 +1,38 @@
+use axum::{
+  async_trait,
+  extract::{FromRequest, RequestParts},
+  http::StatusCode,
+  BoxError, Json,
+};
+use serde::de::DeserializeOwned;
+use validator::Validate;
+
 pub mod todo;
+
+#[derive(Debug)]
+pub struct ValidatedJson<T>(T);
+
+
+#[async_trait] // trait で async を実装するとき
+impl<T, B> FromRequest<B> for ValidatedJson<T>
+where
+    // where はジェネリック境界を宣言
+    T: DeserializeOwned + Validate,
+    B: http_body::Body + Send,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+{
+    type Rejection = (StatusCode, String); // FromRequestに必要でエラーのレスポンス型
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let Json(value) = Json::<T>::from_request(req).await.map_err(|rejection| {
+            let message = format!("Json parse error: [{}]", rejection);
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        value.validate().map_err(|rejection| {
+            let message = format!("Validation eeror: [{}]", rejection).replace('\n', ", ");
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        Ok(ValidatedJson(value))
+    }
+}
--- todo/todo-api/src/handlers/todo.rs
+++ todo/todo-api/src/handlers/todo.rs
@@ -1,41 +1,12 @@
 use crate::repositories::todo::{CreateTodo, TodoRepository, UpdateTodo};
 use axum::{
-    async_trait,
-    extract::{Extension, FromRequest, Path, RequestParts},
+    extract::{Extension, Path},
     http::StatusCode,
     response::IntoResponse,
-    BoxError, Json,
+    Json,
 };
-use serde::de::DeserializeOwned;
 use std::sync::Arc;
-use validator::Validate;
-
-#[derive(Debug)]
-pub struct ValidatedJson<T>(T);
-
-#[async_trait] // trait で async を実装するとき
-impl<T, B> FromRequest<B> for ValidatedJson<T>
-where
-    // where はジェネリック境界を宣言
-    T: DeserializeOwned + Validate,
-    B: http_body::Body + Send,
-    B::Data: Send,
-    B::Error: Into<BoxError>,
-{
-    type Rejection = (StatusCode, String); // FromRequestに必要でエラーのレスポンス型
-
-    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        let Json(value) = Json::<T>::from_request(req).await.map_err(|rejection| {
-            let message = format!("Json parse error: [{}]", rejection);
-            (StatusCode::BAD_REQUEST, message)
-        })?;
-        value.validate().map_err(|rejection| {
-            let message = format!("Validation eeror: [{}]", rejection).replace('\n', ", ");
-            (StatusCode::BAD_REQUEST, message)
-        })?;
-        Ok(ValidatedJson(value))
-    }
-}
+use super::ValidatedJson;
 
 pub async fn create_todo<T: TodoRepository>(
     ValidatedJson(payload): ValidatedJson<CreateTodo>, // request は deserialize
