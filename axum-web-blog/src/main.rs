54df4a981ce45b3f1be43dc2217db96ef2cdf355
sntkn <mail.tsurumi@gmail.com>
2023-02-07 22:22:59 +0900

    HelloWorld

--- axum-web-blog/src/main.rs
+++ axum-web-blog/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let router = Router::new().route("/", get(hello_world));
+    axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_world() -> Json<HelloWorld> {
+    let hello = HelloWorld {
+        text: "Hello World".to_string(),
+    };
+    Json(hello)
+}
+
+#[derive(Serialize)]
+struct HelloWorld {
+    text: String,
 }
