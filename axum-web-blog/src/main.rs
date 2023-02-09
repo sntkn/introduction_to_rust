c64d0bc9ddca4ca52bfc1484b9df21f3b4a9a7b8
sntkn <mail.tsurumi@gmail.com>
2023-02-09 22:04:20 +0900

    create post

--- axum-web-blog/src/main.rs
+++ axum-web-blog/src/main.rs
@@ -1,11 +1,17 @@
-use axum::{routing::get, Json, Router};
-use serde::Serialize;
+use axum::{
+    extract,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/posts", post(create_post));
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
         .await
@@ -22,4 +28,24 @@ async fn hello_world() -> Json<HelloWorld> {
 #[derive(Serialize)]
 struct HelloWorld {
     text: String,
+}
+
+#[derive(Deserialize)]
+struct CreatePost {
+    title: String,
+    body: String,
+}
+
+#[derive(Serialize)]
+struct Post {
+    title: String,
+    body: String,
+}
+
+async fn create_post(extract::Json(payload): extract::Json<CreatePost>) -> Json<Post> {
+    let post = Post {
+        title: payload.title.to_string(),
+        body: payload.body.to_string(),
+    };
+    Json(post)
 }
