71f95499f2db6d464e79fc73de295fb0ac7f3ced
sntkn <mail.tsurumi@gmail.com>
2023-02-15 19:58:52 +0900

    Add gitignore

--- rpn/target/debug/deps/rpn-c778272d3599af31.d
+++
@@ -1,5 +0,0 @@
-/Users/tsurumishouichi/ghq/github.com/sntkn/introduction_to_rust/rpn/target/debug/deps/rpn-c778272d3599af31: src/main.rs
-
-/Users/tsurumishouichi/ghq/github.com/sntkn/introduction_to_rust/rpn/target/debug/deps/rpn-c778272d3599af31.d: src/main.rs
-
-src/main.rs:
--- rpn/target/debug/rpn.d
+++
@@ -1,1 +0,0 @@
-/Users/tsurumishouichi/ghq/github.com/sntkn/introduction_to_rust/rpn/target/debug/rpn: /Users/tsurumishouichi/ghq/github.com/sntkn/introduction_to_rust/rpn/src/main.rs
