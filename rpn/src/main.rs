1afe495da74e06afde7e13197ac364b8029ea84b
sntkn <mail.tsurumi@gmail.com>
2023-02-15 22:14:30 +0900

    Add RpnCalcurator

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -16,6 +16,53 @@ struct Opts {
     #[arg(name = "FILE")]
     formula_file: Option<String>,
 }
+
+struct RpnCalcurator(bool);
+
+impl RpnCalcurator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        // スペースで分割して逆順にしてVec型に変換
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    pub fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                // 数字は stack に追加する
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                // 計算結果を stack に追加する
+                stack.push(res);
+            }
+            // 無名フィールドなら
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
 fn main() {
     let opts = Opts::parse();
 
@@ -31,8 +78,10 @@ fn main() {
 }
 
 fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalcurator::new(verbose);
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
