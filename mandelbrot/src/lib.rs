c368b2216b44c6da5af7a99420a6832addffe72c
sntkn <mail.tsurumi@gmail.com>
2023-02-21 22:49:13 +0900

    Add macro definition

--- mandelbrot/src/lib.rs
+++ mandelbrot/src/lib.rs
@@ -13,10 +13,20 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mandelbrot!");
+macro_rules! console_log {
+    ($t:tt,$s:block) => {{
+        let window = web_sys::window().expect("should have a window in this context");
+        let performance = window
+            .performance()
+            .expect("performance should be available");
+        let start = performance.now();
+        let result = { $s };
+        let end = performance.now();
+        console_log!("{}:{}[ms]", $t, end - start);
+        result
+    }};
 }
