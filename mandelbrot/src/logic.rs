387d3ec906c76384d20f2bfed8321a45fc69bf68
sntkn <mail.tsurumi@gmail.com>
2023-02-21 22:15:22 +0900

    Add test

--- mandelbrot/src/logic.rs
+++ mandelbrot/src/logic.rs
@@ -11,7 +11,7 @@ fn get_n_diverged(x0: f64, y0: f64, max_iter: usize) -> u8 {
             return i as u8;
         }
     }
-    max_iter as u8;
+    max_iter as u8
 }
 
 pub fn generate_mandelbrot_set(
@@ -39,5 +39,34 @@ pub fn generate_mandelbrot_set(
             data.push(255);
         }
     }
-    data;
+    data
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_n_diverged() {
+        let max_iter = 10;
+        assert_eq!(get_n_diverged(1.0, 0.0, max_iter), 3); //0->1->2->5
+        assert_eq!(get_n_diverged(0.0, 0.0, max_iter), max_iter as u8);
+        assert_eq!(get_n_diverged(0.0, 1.0, max_iter), max_iter as u8);
+    }
+
+    #[test]
+    fn test_generate_mendelbrot_set() {
+        let canvas_w = 2;
+        let canvas_h = 2;
+        let x_min = -1.0;
+        let x_max = 1.0;
+        let y_min = -1.0;
+        let y_max = 1.0;
+        let max_iter = 8;
+        // (x, y) in [(-1, -1), (-1, 0), (0,-1), (0,0)]
+        assert_eq!(
+            generate_mandelbrot_set(canvas_w, canvas_h, x_min, x_max, y_min, y_max, max_iter),
+            vec![96, 96, 96, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255]
+        )
+    }
 }
