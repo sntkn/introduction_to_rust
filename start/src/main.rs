0f1e79ca4a98a73b8466b584a0fae65f7d1fe0e2
sntkn <mail.tsurumi@gmail.com>
2023-03-12 15:41:24 +0900

    add comment

--- start/src/main.rs
+++ start/src/main.rs
@@ -8,6 +8,33 @@ fn main() {
     func_ex_print_result(func_ex_div_result(10, 5));
     func_ex_print_result(func_ex_div_result(10, 0));
     crate::module_hello::print_hello();
+    let s = "Hello".to_string();
+    myprint(&s); // リファレンスとして渡す
+
+    // イミュータブルな変数は２つリファレンスすることはできない
+    let mut s = "Hello".to_string();
+    let s_ref1 = &mut s;
+    //let s_ref2 = &mut s; // mutable more than once at a time
+    myclear(s_ref1);
+}
+
+fn _pick(x: &[i32], end: usize) -> &[i32] {
+    // let x = [1, 2, 3, 4]; // ローカル変数をリファレンスとして返却できない
+    &x[..end]
+}
+
+// 複数のリファレンスを含む返却値はライフタイムパラメータとジェネリクスパラメータ（&'a）をつける
+fn _pick2<'a, 'b>(x: &'a [i32], y: &'b [i32], end: usize) -> (&'a [i32], &'b [i32]) {
+    (&x[..end], &y[..end])
+}
+fn myprint<T: std::fmt::Display>(msg: &T) {
+    // リファレンスとして受け取る
+    println!("{}", *msg); // でリファレンスとして実行
+}
+
+fn myclear(x: &mut String) {
+    // リファレンス＆ミュータブルとして受け取る
+    x.clear();
 }
 
 fn func_ex_div_some(x: i32, y: i32) -> Option<i32> {
